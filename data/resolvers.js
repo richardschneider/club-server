e1c2085dbb05064fde0c9c1641fd28ec6d2ad3ee
Richard Schneider <makaretu@gmail.com>
2017-06-16 15:09:40 +1200

    refactor(game): a feature based project directory

--- data/resolvers.js
+++ data/resolvers.js
@@ -88,42 +88,7 @@ const resolvers = {
   },
 
   Board: require('../lib/board/resolver'),
-
-  Game: {
-    board(game) {
-      return game.getBoard();
-    },
-    contract(game) {
-      return {
-        level: game.level,
-        denomination: game.denomination,
-        risk: game.risk,
-        declaror: game.declaror,
-      };
-    },
-    NS(game) {
-      return {
-          game: game,
-          pairNumber: game.ns,
-          direction: 'NS',
-          score: (game.declaror === 'N' || game.declaror === 'S') ? game.score : -game.score,
-          matchpoints: game.matchpointsNS,
-          matchpointsPercentage: game.matchpointsPercentageNS,
-          imps: game.impsNS,
-      };
-    },
-    EW(game) {
-      return {
-          game: game,
-          pairNumber: game.ew,
-          direction: 'EW',
-          score: (game.declaror === 'E' || game.declaror === 'W') ? game.score : -game.score,
-          matchpoints: game.matchpointsEW,
-          matchpointsPercentage: game.matchpointsPercentageEW,
-          imps: game.impsEW
-      };
-    },
-  },
+  Game: require('../lib/game/resolver'),
 
   GamePairResult: {
     pair(gamePairResult) {
--- data/schema.js
+++ data/schema.js
@@ -57,22 +57,7 @@ type SessionPair {
  }
 
 ${require('../lib/board/graphql-schema')}
-
-type Game {
-  id: ID
-  board: Board
-  contract: Contract
-  lead: String
-
-  # the declaror's score based on the contract and made tricks
-  score: Int
-
-  # the number of tricks made over the book contract (6) or a negative number indicating the number of tricks down on the contract
-  made: Int
-
-  NS: GamePairResult
-  EW: GamePairResult
-}
+${require('../lib/game/graphql-schema')}
 
 type GamePairResult {
   pair: SessionPair
